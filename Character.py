a2311c21a784842785bf466b8f94ae6d47cc3ce4
gregk <gpkort@gmail.com>
2018-08-30 14:44:03 -0600

    Added name funtion

--- Character.py
+++ Character.py
@@ -1,9 +1,20 @@
 
 class Character(object):
+    MAX_ATTRIBUTE_VALUE = 18
+    MIN_ATTRIBUTE_VALUE = 3
 
-    def __init__(self, strength: int):
-        self.__strength = strength
+    def __init__(self, name: str):
+        self.__strength = 0
+        self.__intelligence = 0
+        self.__wisdom = 0
+        self.__dexterity = 0
+        self.__charisma = 0
+        self.__constitution = 0
 
+        self.validate_name(name)
+        self.__name = name
+
+# <editor-fold desc="Attributes properties>
 
     @property
     def strength(self):
@@ -11,5 +22,77 @@ class Character(object):
 
     @strength.setter
     def strength(self, val):
-        pass
+        self.validate_attribute(val)
+        self.__strength = val
 
+    @property
+    def intelligence(self):
+        return self.__intelligence
+
+    @intelligence.setter
+    def intelligence(self, val):
+        self.validate_attribute(val)
+        self.__intelligence = val
+
+    @property
+    def wisdom(self):
+        return self.__wisdom
+
+    @wisdom.setter
+    def wisdom(self, val):
+        self.validate_attribute(val)
+        self.__wisdom = val
+
+    @property
+    def dexterity(self):
+        return self.__dexterity
+
+    @dexterity.setter
+    def dexterity(self, val):
+        self.validate_attribute(val)
+        self.__dexterity = val
+
+    @property
+    def charisma(self):
+        return self.__charisma
+
+    @charisma.setter
+    def charisma(self, val):
+        self.validate_attribute(val)
+        self.__charisma = val
+
+    @property
+    def constitution(self):
+        return self.__constitution
+
+    @constitution.setter
+    def constitution(self, val):
+        self.validate_attribute(val)
+        self.__constitution = val
+
+# </editor-fold>
+
+    @property
+    def Name(self) -> str:
+        return self.__name
+
+    @Name.setter
+    def Name(self, val: str):
+        self.validate_name(val)
+        self.__name = val
+
+    @staticmethod
+    def validate_attribute(val: int):
+        if type(val) is not int:
+            raise ValueError("Attribute must be an integer")
+        if val < Character.MIN_ATTRIBUTE_VALUE:
+            raise ValueError("Attribute must be greater than {}".format(Character.MIN_ATTRIBUTE_VALUE))
+        if val > Character.MAX_ATTRIBUTE_VALUE:
+            raise ValueError("Attribute must be less than {}".format(Character.MAX_ATTRIBUTE_VALUE))
+
+    @staticmethod
+    def validate_name(val: str):
+        if type(val) is not str:
+            raise ValueError("Name must be a string")
+        if len(val) == 0:
+            raise ValueError("Name cannot be empty")
--- UnitTests/Test_Character.py
+++ UnitTests/Test_Character.py
@@ -2,9 +2,97 @@ import unittest
 from Character import Character
 
 
-
 class TestsCharacter(unittest.TestCase):
+    def setUp(self):
+        self.testChar = Character(name="Greg")
 
-    def test_strength_init(self):
-        character = Character(3)
-        self.assertEqual(character.strength, 3)
+    def test__init(self):
+        self.assertEqual(self.testChar.strength, 0, "strength ctor test failed")
+        self.assertEqual(self.testChar.intelligence, 0, "intelligence ctor test failed")
+        self.assertEqual(self.testChar.wisdom, 0, "wisdom ctor test failed")
+        self.assertEqual(self.testChar.dexterity, 0, "dexterity ctor test failed")
+        self.assertEqual(self.testChar.charisma, 0, "charisma ctor test failed")
+        self.assertEqual(self.testChar.constitution, 0, "constitution ctor test failed")
+
+        self.assertEqual(self.testChar.Name, "Greg", "name ctor test failed")
+
+# <editor-fold desc="Attributes tests">
+
+    def test_strengthTooLow(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "strength", 0)
+
+    def test_strengthTooHigh(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "strength", 20)
+
+    def test_strengthNotInt(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "strength", 0.5)
+
+    # Intelligence Tests
+
+    def test_intelligenceTooLow(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "intelligence", 0)
+
+    def test_intelligenceTooHigh(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "intelligence", 20)
+
+    def test_intelligenceNotInt(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "intelligence", 0.5)
+
+    # Wisdom Tests
+
+    def test_wisdomTooLow(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "wisdom", 0)
+
+    def test_wisdomTooHigh(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "wisdom", 20)
+
+    def test_wisdomNotInt(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "wisdom", 0.5)
+
+    # dexterity Tests
+
+    def test_dexterityTooLow(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "dexterity", 0)
+
+    def test_dexterityTooHigh(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "dexterity", 20)
+
+    def test_dexterityNotInt(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "dexterity", 0.5)
+
+    # charisma Tests
+
+    def test_charismaTooLow(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "charisma", 0)
+
+    def test_charismaTooHigh(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "charisma", 20)
+
+    def test_charismaNotInt(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "charisma", 0.5)
+
+    # charisma Tests
+
+    def test_constitutionTooLow(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "constitution", 0)
+
+    def test_constitutionTooHigh(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "constitution", 20)
+
+    def test_constitutionNotInt(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "constitution", 0.5)
+# </editor-fold>
+
+    def test_nameNotStr(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "Name", 20)
+
+    def test_nameEmpty(self):
+        self.assertRaises(ValueError, setattr, self.testChar, "Name", "")
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+# @unittest.expectedFailure
